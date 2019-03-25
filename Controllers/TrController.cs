e38244416f8f97c58b943804a870592ab5c67e34
Hasan KELİCİ <hasankelici@msn.com>
2019-03-25 14:45:00 +0300

    first commit

---
+++ Controllers/TrController.cs
@@ -0,0 +1,102 @@
+﻿using BRC.Derya.Business.Abstract;
+using BRC.Derya.DataAccsess.Abstract;
+using BRC.Derya.Entities;
+using BRC.Derya.WebUI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BRC.Derya.WebUI.Controllers
+{
+    public class TrController : Controller
+    {
+        private IIletisimService _iletisimService;
+        private IGaleriService _galeriService;
+        private IKullaniciService _kullaniciService;
+        private IMedyaService _medyaService;
+        private ISliderService _sliderService;
+        private IUrunlerService _urunlerService;
+
+
+        public TrController(
+            IIletisimService iletisimService,
+            IGaleriService galeriService,
+            IKullaniciService kullaniciService,
+            IMedyaService medyaService,
+            ISliderService sliderService,
+            IUrunlerService urunlerService)
+        {
+            _iletisimService = iletisimService;
+            _galeriService = galeriService;
+            _iletisimService = iletisimService;
+            _kullaniciService = kullaniciService;
+            _medyaService = medyaService;
+            _sliderService = sliderService;
+            _urunlerService = urunlerService;
+        }
+        // GET: Tr
+        public ActionResult Index()
+        {
+            ViewBag.OnlineVisitor = HttpContext.Application["OnlineVisitor"];
+            return View();
+        }
+        public ActionResult Contact()
+        {
+            ViewBag.OnlineVisitor = HttpContext.Application["OnlineVisitor"];
+            //var il = _iletisimService.GetAll();
+            //IletisimListViewModel model
+            //= new IletisimListViewModel
+            //{
+            //    iletisims = il
+            //};
+            //return View(model);
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Contact(iletisim iletisim)
+        {
+            ViewBag.OnlineVisitor = HttpContext.Application["OnlineVisitor"];
+            _iletisimService.Add(iletisim);
+            return View();
+        }
+        public ActionResult Products()
+        {
+            ViewBag.OnlineVisitor = HttpContext.Application["OnlineVisitor"];
+            var urunler = _urunlerService.GetAll();
+            UrunlerListViewModel model
+            = new UrunlerListViewModel 
+            {
+                Urunlers=urunler
+            };
+            return View(model);
+        }
+        public ActionResult Gallery()
+        {
+            ViewBag.OnlineVisitor = HttpContext.Application["OnlineVisitor"];
+            var galeri= _galeriService.GetAll();
+            GaleriListViewModel galeriListViewModel 
+            = new GaleriListViewModel
+            {
+                Galeris = galeri
+            };
+            return View(galeriListViewModel);
+        }
+        public ActionResult Media()
+        {
+            ViewBag.OnlineVisitor = HttpContext.Application["OnlineVisitor"];
+            var medya = _medyaService.GetAll();
+            MedyaListViewModel model   
+            = new MedyaListViewModel
+            {
+                Medyas = medya
+            };
+            return View(model);
+        }
+        public ActionResult test()
+        {
+            return View();
+        }
+    }
+}
---
+++ Global.asax.cs
@@ -0,0 +1,59 @@
+﻿using Autofac;
+using Autofac.Integration.Mvc;
+using BRC.Derya.Business.Abstract;
+using BRC.Derya.Business.Concrete;
+using BRC.Derya.DataAccsess.Abstract;
+using BRC.Derya.DataAccsess.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace BRC.Derya.WebUI
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterControllers(Assembly.GetExecutingAssembly());
+            
+            builder.RegisterType<IletisimManager>().As<IIletisimService>();
+            builder.RegisterType<EfIletisimDal>().As<IIletisimDal>();
+            builder.RegisterType<GaleriManager>().As<IGaleriService>();
+            builder.RegisterType<EfGaleriDal>().As<IGaleriDal>();
+            builder.RegisterType<KullaniciManager>().As<IKullaniciService>();
+            builder.RegisterType<EfKullaniciDal>().As<IKUllaniciDal>();
+            builder.RegisterType<MedyaManager>().As<IMedyaService>();
+            builder.RegisterType<EfMedyaDal>().As<IMedyaDal>();
+            builder.RegisterType<SliderManager>().As<ISliderService>();
+            builder.RegisterType<EfSliderDal>().As<ISliderDal>();
+            builder.RegisterType<UrunlerManager>().As<IUrunlerService>();
+            builder.RegisterType<EfUrunlerDal>().As<IUrunlerDal>();
+
+            IContainer container = builder.Build();
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+
+            AreaRegistration.RegisterAllAreas();
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            //test
+           
+        }
+        protected void Session_Start(object sender,EventArgs e)
+        {
+            int onlineUye = Convert.ToInt32(Application["OnlineVisitor"]);
+            onlineUye = onlineUye + 1;
+            Application["OnlineVisitor"] = onlineUye;
+        }
+        protected void Session_End(object sender, EventArgs e)
+        {
+
+            int onlineUye = Convert.ToInt32(Application["onuye"]);
+            onlineUye = onlineUye - 1;
+            Application["OnlineVisitor"] = onlineUye;
+        }
+    }
+}
